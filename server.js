b0b537feab65687843cf8e517d010f763cd65a2f
Jeremy <jeremy.zaleta@98labs.com>
2020-05-27 02:08:50 +0800

    fixed server js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/speech-management-app'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/speech-management-app/index.html'));});
+
+app.use(express.static('./dist/speech-management-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/speech-management-app/'}),
+);
+
 app.listen(process.env.PORT || 8080);
