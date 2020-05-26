d5fa22d179061f0af07ebfe436c4cef3698b0033
Jeremy <jeremy.zaleta@98labs.com>
2020-05-27 02:58:39 +0800

    update server js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/speech-management-app/dist'));
-app.get('*', (req, res) => {
-  res.sendFile(`./speech-management-app/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
+app.use(express.static(__dirname + '/dist/speech-management-app'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/speech-management-app/index.html'));});
 app.listen(process.env.PORT || 8080);
